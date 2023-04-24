1efe963a8dad77612a3accffb4fab89c0a734f2b
Richsl <42124330+Richsl@users.noreply.github.com>
2023-04-23 17:31:35 -0700

    FM and AM working

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@ AudioKit kit;
 //SineWaveGenerator wave;
 //MultiWaveGenerator wave; // uses default wave settings
 // MultiWaveGenerator(float amplitude = 32767.0, float phase = 0.0, waveType wt = WAV_SINE, float pwm = PI,int invert = 0)
-MultiWaveGenerator carrier_wave(32767.0,0.0,WAV_SINE,2*PI,0);
-MultiWaveGenerator mod_wave(32767.0,0.0,WAV_PWM,PI,0);
+MultiWaveGenerator CAR_wave(32767.0,0.0,WAV_SINE,2*PI,0);
+MultiWaveGenerator FM_wave(32767.0,0.0,WAV_SINE,2*PI,0);
+MultiWaveGenerator AM_wave(32767.0,0.0,WAV_SINE,2*PI,0);
 const int BUFFER_SIZE = 1024; // this is the buffer size in bytes, not samples
 uint8_t buffer[BUFFER_SIZE];
 uint8_t MTbuffer[BUFFER_SIZE];
@@ -18,17 +19,45 @@ const int DELAY_BUFFER_SIZE = 24350*4; // one second of delay with four bytes pe
 uint8_t delay_buffer[DELAY_BUFFER_SIZE];
 float maxamp =32767.0;
 int DELAY = 1020;
-uint16_t carrier_freq =3000;
-uint16_t mod_freq =6;
+uint16_t FM_WF=0;
+float FM_FQ =6;
+float FM_AMT =0;
+uint16_t CAR_FQ =3000;
+uint16_t AM_WF =0;
+float AM_FQ =0;
+float AM_AMT =0;
+uint16_t CAR_VOL =0;
+uint16_t DEL_RT =23000;
+float DEL_AMT=0;
+float DEL_FB=0;
+int16_t FM_instant;
+int16_t AM_instant;
+float AM_float;
+uint16_t CAR_FQ_READ;
+uint16_t CAR_FQ_READ_NEW;
+
+
 size_t bytes2i2s;
 int16_t* del_ptr_write;
 int16_t* del_ptr_read;
 int16_t* del_ptr_start;
 int16_t* del_ptr_end;
 
-const int firePin = 36;  // the number of the pushbutton pin should re 'rec' button on LyraT
+const int firePin = 12;  // the number of the pushbutton pin should re 'rec' button on LyraT
 int fireState = 1;  // input goes low when fired
-const int carrierFreqPotPin = 15;
+
+const int FM_WF_PIN = 13;
+const int FM_FQ_PIN = 39;
+const int FM_AMT_PIN = 34;
+const int CAR_FQ_PIN = 32;
+const int AM_WF_PIN = 33;
+const int AM_FQ_PIN = 27;
+const int AM_AMT_PIN = 14;
+const int CAR_VOL_PIN = 15;
+const int DEL_RT_PIN = 36;
+const int DEL_AMT_PIN = 4;
+const int DEL_FB_PIN = 2;
+
 int freqPotValue=1;
 
 void setup() {
@@ -46,7 +75,7 @@ void setup() {
   // open in write mode
   auto cfg = kit.defaultConfig(AudioOutput);
   kit.begin(cfg);
-  kit.setVolume(40);
+  kit.setVolume(30);
 
   del_ptr_write = (int16_t*)delay_buffer;
   del_ptr_start = (int16_t*)delay_buffer;
@@ -62,33 +91,62 @@ void setup() {
   Serial.print((int)del_ptr_read);
     Serial.print("del_ptr_write/n");
   Serial.print((int)del_ptr_write);
-  pinMode(firePin, INPUT);
+  pinMode(firePin, INPUT_PULLUP);
+  //CLEAR_PERI_REG_MASK(USB_SERIAL_JTAG_CONF0_REG, USB_SERIAL_JTAG_DP_PULLUP);
   
 
   // 1000 hz
-  carrier_wave.setFrequency(carrier_freq);
-  carrier_wave.setSampleRate(cfg.sampleRate());
-  mod_wave.setFrequency(mod_freq);
-  mod_wave.setSampleRate(cfg.sampleRate());
+  CAR_wave.setFrequency(CAR_FQ);
+  CAR_wave.setSampleRate(cfg.sampleRate());
+  AM_wave.setFrequency(AM_FQ);
+  AM_wave.setSampleRate(cfg.sampleRate());
+  FM_wave.setFrequency(FM_FQ);
+  FM_wave.setSampleRate(cfg.sampleRate());
   //size_t bufferfilledsize = wave.read(buffer, BUFFER_SIZE);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  freqPotValue = analogRead(carrierFreqPotPin);
-  carrier_wave.setFrequency(freqPotValue+100);
+
+  FM_AMT = 1-((float)analogRead(FM_AMT_PIN)/4096);
+  AM_AMT = 1-((float)analogRead(AM_AMT_PIN)/4096);
+  AM_FQ = 20*(((float)(4096-analogRead(AM_FQ_PIN)))/4096);
+  AM_wave.setFrequency(AM_FQ);
+  FM_FQ = 20*(((float)(4096-analogRead(FM_FQ_PIN)))/4096);
+  FM_wave.setFrequency(FM_FQ);
+  CAR_FQ_READ_NEW = analogRead(CAR_FQ_PIN);
+  if (abs(CAR_FQ_READ_NEW-CAR_FQ_READ)>150){
+    CAR_FQ_READ=CAR_FQ_READ_NEW;
+  }
+  //FM_instant = FM_AMT*100*(float)(FM_wave.readSample2()/ 32767.0);
+  //FM_instant = FM_wave.readSample2();
+  //AM_instant = AM_wave.readSample2();
+  //CAR_FQ = (FM_instant)+100+(4095- analogRead(CAR_FQ_PIN));
+  //CAR_FQ = (10*(FM_AMT*FM_instant/ 32767.0))+100+(4095- analogRead(CAR_FQ_PIN));
+  //CAR_wave.setFrequency(CAR_FQ);
   size_t bytes =  BUFFER_SIZE;
   size_t result = 0;
   int16_t *ptr = (int16_t*)buffer;
   fireState = digitalRead(firePin); 
   for (int j=0;j<bytes/4;j++){
-       int16_t sample =  (mod_wave.readSample2()/ 32767.0) * carrier_wave.readSample2();
+        FM_instant = FM_wave.readSample2();
+        AM_instant = AM_wave.readSample2();
+        //AM_float = (1-(AM_AMT*((float)AM_instant/32767)));
+        AM_float = (AM_AMT)*((float)AM_instant/32767); // -1 to 1
+        AM_float = 0.5f * AM_float; // 0.5 to -0.5
+        AM_float = AM_float + 0.5; // 0 to 1
+        AM_float = 1.5 - (AM_AMT/2) - AM_float;
+        CAR_FQ = (200*(FM_AMT*FM_instant/ 32767.0))+100+(4095- CAR_FQ_READ);
+        CAR_wave.setFrequency(CAR_FQ);
+       //int16_t sample =  (1-(AM_AMT*AM_wave.readSample2()/ 32767.0)) * CAR_wave.readSample2();
+       //int16_t sample =   (int)((1-(AM_AMT*AM_instant/ 32767.0)) * (float)CAR_wave.readSample2());
+       int16_t sample =   (int16_t)(AM_float * (float)CAR_wave.readSample2());
        if (fireState==HIGH)
        {sample=0;}
        int16_t delay_sample = *del_ptr_read++;
        delay_sample = *del_ptr_read++;
-       int16_t mix = (sample/2) + (delay_sample/2);
-       //int16_t mix = sample;
+       //int16_t mix = (sample/2) + (delay_sample/2);
+       int16_t mix = sample;
       
       *ptr++ = mix;
       *ptr++ = mix;
@@ -116,6 +174,20 @@ void loop() {
     // fire button is NOT pressed, no sound
      //bytes2i2s=kit.write(MTbuffer, 1024);
   } else {
+  //   Serial.print("PUshez\n");
+  //   Serial.print("CAR_FQ\n");
+  // Serial.print((int)CAR_FQ);
+  //     Serial.print("\nFM_FQ\n");
+   //Serial.print(FM_FQ);
+         Serial.print("\nAM_float\n");
+   Serial.print(AM_float);
+  //         Serial.print("\nFM_instant\n");
+  // Serial.print(FM_instant);
+  //       Serial.print("\nAM_FQ\n");
+  // Serial.print(AM_FQ);
+  //           Serial.print("\nAM_instant\n");
+  // Serial.print(AM_instant);
+
     // fire button is  pressed, sound on
     // bytes2i2s=kit.write(buffer, 1024);
   }
@@ -134,12 +206,12 @@ void loop() {
   if (DELAY < 1)
   {
     // print this somehow for debug float i2s_get_clk(i2s_port_t i2s_num);
-    int DELAY = 10;
-    carrier_freq= carrier_freq +100;
-    carrier_wave.setFrequency(carrier_freq);
-    if (carrier_freq > 5000){
-      carrier_freq =300;
-    }
+    // int DELAY = 10;
+    // carrier_freq= carrier_freq +100;
+    // carrier_wave.setFrequency(carrier_freq);
+    // if (carrier_freq > 5000){
+    //   carrier_freq =300;
+    // }
 
 
   }
